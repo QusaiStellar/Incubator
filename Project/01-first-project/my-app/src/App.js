7e2e2e8477617a200f4f0f53441dc1d24373bc3b
Andrew Dobrik <omgdobrik@gmail.com>
2022-03-27 20:03:50 +0300

    go

--- Project/01-first-project/my-app/src/App.js
+++ Project/01-first-project/my-app/src/App.js
@@ -1,14 +1,46 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 const App = () => {
    return (
-      <div className="App">
-         <ul>
-            <li>css</li>
-            <li>html</li>
-            <li>js</li>
-         </ul>
+      <div className='app-wrapper'>
+
+         <header className='header'>
+            <img src={require('./img/logo.png')} />
+         </header>
+
+         <nav className='navigation'>
+            <ul>
+               <li><a>Profile</a></li>
+               <li><a>Messages</a></li>
+               <li><a>News</a></li>
+               <li><a>Musics</a></li>
+               <li><a>Settings</a></li>
+            </ul>
+         </nav>
+
+         <main className='content'>
+            <div className='content__background'>
+               <img src={require('./img/main-bg.jpg')} />
+            </div>
+            <div className='user'>
+               <div className='user__avatar'>
+                  <img src={require('./img/avatar.jpg')} />
+               </div>
+               <div className='user__allinfo'>
+                  <div className='user__name'>Andrew Dobrik</div>
+                  <div className='user__info'>
+                     <ul>
+                        <li>Date of birthday: 26 april</li>
+                        <li>City: Stolbtsy</li>
+                        <li>Education: BNTU '18</li>
+                        <li>Web-site: none</li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+         </main>
+
       </div>
    );
 }
