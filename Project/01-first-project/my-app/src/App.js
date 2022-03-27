a99cea58ecb38932d20ddb1f982c3f67c34d2838
Andrew Dobrik <omgdobrik@gmail.com>
2022-03-27 16:57:40 +0300

    Initial commit

---
+++ Project/01-first-project/my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+   return (
+      <div className="App">
+         <ul>
+            <li>css</li>
+            <li>html</li>
+            <li>js</li>
+         </ul>
+      </div>
+   );
+}
+
+export default App;
