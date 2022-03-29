9d377283b7102bf0e19ff2af1e2000c478e10472
Andrew Dobrik <omgdobrik@gmail.com>
2022-03-29 11:38:05 +0300

    props

--- Project/01-first-project/my-app/src/components/Content/Posts/Posts.jsx
+++ Project/01-first-project/my-app/src/components/Content/Posts/Posts.jsx
@@ -5,13 +5,12 @@ import styles from './Posts.module.css';
 
 const Posts = () => {
    return (
-
       <div className={styles.posts}>
-
-         <Post />
-         <Post />
-         <Post />
-
+         <Post text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id, voluptatum blanditiis, 
+         rerum odio vel nisi quaerat necessitatibus vitae facilis perspiciatis itaque doloremque iure nam 
+         porro illo in pariatur et ea?"/>
+         <Post text="Lorem ipsum dolor sit amet consectetur, adipisicing elit." />
+         <Post text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ex cupiditate nulla, nam maxime sint odio eligendi aliquid necessitatibus ut?" />
       </div>
 
    );
