2ec65dd454755cb1c12eea02549fbda437d371ff
Andrew Dobrik <omgdobrik@gmail.com>
2022-05-05 16:06:29 +0300

    componentDidMount

--- Project/01-first-project/my-app/src/components/Users/Users.jsx
+++ Project/01-first-project/my-app/src/components/Users/Users.jsx
@@ -6,39 +6,35 @@ import userImg from '../../img/avatar.jpg';
 import styles from './Users.module.css';
 
 class Users extends React.Component {
-
-   setUsers = () => {
-      if (this.props.users.length === 0) {
-         axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then(response => this.props.setUsers(response.data.items));
-      }
-   };
    
+
+   componentDidMount() {
+      axios.get('https://social-network.samuraijs.com/api/1.0/users')
+         .then(response => this.props.setUsers(response.data.items));
+   }
+
    render = () => {
       return (
-         <div className={styles.users}>
-            <button onClick={this.setUsers} className={styles.getUsers}>Get users</button>
-            <div className={styles.wrapper}>
-               {this.props.users.map(users =>
-                  <section className={styles.usersWrapper}>
-                     <div className={styles.avatar}>
-                        <img src={users.photos.small != null ? users.photos.small : userImg} className={styles.userAvatar} alt="avatar" />
-                        <button onClick={users.followed ? () => { this.props.unfollow(users.id); } : () => { this.props.follow(users.id); }} className={styles.btn}>{users.followed ? 'Unfollow' : 'Follow'}</button>
+         <div className={styles.wrapper}>
+            {this.props.users.map(users =>
+               <section className={styles.usersWrapper}>
+                  <div className={styles.avatar}>
+                     <img src={users.photos.small != null ? users.photos.small : userImg} className={styles.userAvatar} alt="avatar" />
+                     <button onClick={users.followed ? () => { this.props.unfollow(users.id); } : () => { this.props.follow(users.id); }} className={styles.btn}>{users.followed ? 'Unfollow' : 'Follow'}</button>
+                  </div>
+                  <div className={styles.allInfo}>
+                     <div className={styles.userInfo}>
+                        <p className={styles.fullName}>{users.name}</p>
+                        <p className={styles.status}>{users.status}</p>
                      </div>
-                     <div className={styles.allInfo}>
-                        <div className={styles.userInfo}>
-                           <p className={styles.fullName}>{users.name}</p>
-                           <p className={styles.status}>{users.status}</p>
-                        </div>
-                        <div className={styles.location}>
-                           <p className={styles.country}>Belarus</p>
-                           <p className={styles.city}>Stowbtsy</p>
-                        </div>
+                     <div className={styles.location}>
+                        <p className={styles.country}>Belarus</p>
+                        <p className={styles.city}>Stowbtsy</p>
                      </div>
-                  </section >
-               )
-               }
-            </div>
+                  </div>
+               </section >
+            )
+            }
          </div>
       );
    };
