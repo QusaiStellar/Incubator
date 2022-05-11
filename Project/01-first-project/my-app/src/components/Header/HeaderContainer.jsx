6ff6d718ace85d4da0942735b3018960c5e42ac0
Andrew Dobrik <omgdobrik@gmail.com>
2022-05-11 19:23:40 +0300

    links

--- Project/01-first-project/my-app/src/components/Header/HeaderContainer.jsx
+++ Project/01-first-project/my-app/src/components/Header/HeaderContainer.jsx
@@ -13,13 +13,13 @@ class HeaderContainer extends React.Component {
    componentDidMount() {
       axios.get('https://social-network.samuraijs.com/api/1.0/auth/me', { withCredentials: true })
          .then(response => {
-
             if (response.data.resultCode === 0) {
                const { id, login, email } = response.data.data;
                this.props.setUserAuth(id, login, email);
             }
          });
    }
+   
    render = () => {
       return (
          <Header {...this.props} />
