1846da5dcffb41ccf9031fa3c42e6301366c1a95
Andrew Dobrik <omgdobrik@gmail.com>
2022-05-22 22:54:04 +0300

    login

--- Project/01-first-project/my-app/src/components/Login/Login.jsx
+++ Project/01-first-project/my-app/src/components/Login/Login.jsx
@@ -1,21 +1,53 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 import styles from './Login.module.scss';
 
-
 const Login = (props) => {
+
+   const {
+      register,
+      handleSubmit,
+      reset,
+      watch,
+      formState: { errors, isValid },
+   } = useForm({
+      mode: 'onBlur',
+   });
+
+   const onSubmit = data => {
+      alert(JSON.stringify(data));
+      reset();
+   };
+
+   console.log(watch('login'));
+
    return (
       <div className={styles.wrapper}>
-         <form className={styles.flex__wrapper}>
+         <form className={styles.flex__wrapper} onSubmit={handleSubmit(onSubmit)}>
             <h3>Login Here</h3>
 
-            <label for="username">Username</label>
-            <input type="text" placeholder="Email or Phone" id="username" />
+            <label >
+               Username
+               <input placeholder="Email or Phone" {...register('login', {
+                  required: true,
+               })} />
+            </label>
+            <div className={styles.error}>
+               {errors?.login && 'Error'}
+            </div>
 
-            <label for="password">Password</label>
-            <input type="password" placeholder="Password" id="password" />
+            <label >
+               Password
+               <input type="password" placeholder="Password" {...register('password', {
+                  required: true,
+               })} />
+            </label>
+            <div className={styles.error}>
+               {errors?.password && 'Error'}
+            </div>
 
-            <button>Log In</button>
+            <input type="submit" value="Log In" className={styles.btn} disabled={!isValid} />
          </form>
       </div>
    );
